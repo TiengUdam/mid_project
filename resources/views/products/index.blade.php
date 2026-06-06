43054a745760e2ead47294d2cff28b15fab5f084
Tieng Udam <tiengudam24@gmail.com>
2026-06-06 19:55:32 +0700

    update UI

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -185,10 +185,11 @@
 </head>
 <body>
 
+    
 <!-- NAVBAR -->
 <nav class="navbar navbar-expand-lg sticky-top">
     <div class="container">
-        <a class="navbar-brand" href="/"><i class="fas fa-mobile-alt me-2"></i>phone dtor</a>
+        <a class="navbar-brand" href="/"><i class="fas fa-mobile-alt me-2"></i>Leader sip</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
